5dfce2113f7bfa59b43048cfced00710208de336
Trey Piepmeier <trey@treypiepmeier.com>
2024-03-02 15:25:27 -0500

    Get backup script to work

--- daily-backup.sh
+++ daily-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker exec -it stacks_django sqlite3 /db/db.sqlite3 '.backup backup.sqlite3'
-docker cp stacks_django:/db/backup.sqlite3 backup.sqlite3
+docker exec -it stacks_django sqlite3 /code/db/db.sqlite3 '.backup /code/db/backup.sqlite3'
+docker cp stacks_django:/code/db/backup.sqlite3 backup.sqlite3
 gzip backup.sqlite3
 s3cmd put backup.sqlite3.gz s3://tp-homeserver-backups/stacks/`date +%F`.sqlite3.gz
 rm backup.sqlite3.gz
