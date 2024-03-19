382cf6978c81e4fff678ad7b63c91bac469f37aa
Trey Piepmeier <trey@treypiepmeier.com>
2024-03-19 08:46:30 -0400

    Sort covers by created_at

--- core/models.py
+++ core/models.py
@@ -129,6 +129,9 @@ class BookCover(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_at"]
+
     def __str__(self):
         return f"Cover of {self.book}"
 
