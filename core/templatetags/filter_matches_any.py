558df357bd08598971e34a855555659208122c59
Trey Piepmeier <trey@treypiepmeier.com>
2024-04-06 17:07:24 -0400

    Ok, genre sub-filters actually working
    I hope. Not sure what I was thinking before.

--- core/templatetags/filter_matches_any.py
+++ core/templatetags/filter_matches_any.py
@@ -3,11 +3,11 @@ from django import template
 register = template.Library()
 
 
-@register.filter
-def filter_matches_any(item, filter_queries):
-    if filter_queries.get(item.name, False):
+@register.simple_tag
+def filter_matches_any(item, filter_name, filter_queries):
+    if filter_queries.get(filter_name, False) == item.slug:
         return True
     for sub_item in item.__class__.objects.filter(parent=item):
-        if filter_matches_any(sub_item, filter_queries):
+        if filter_matches_any(sub_item, filter_name, filter_queries):
             return True
     return False
