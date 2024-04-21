62ebd7cdfae1cb183b6d82d966b96e41ade97aed
Trey Piepmeier <trey@treypiepmeier.com>
2024-04-21 11:44:49 -0400

    Add more view coverage

--- config/tests/test_views.py
+++ config/tests/test_views.py
@@ -2,7 +2,7 @@ import pytest
 from http import HTTPStatus
 from django.urls import reverse
 from model_bakery import baker
-from core.models import Book, BookType, BookGenre
+from core.models import Book, BookType, BookGenre, BookLocation
 
 
 def test_favicon(client):
@@ -71,20 +71,119 @@ def book_data():
     type2 = baker.make(BookType, slug="type2", parent=type1)
     genre1 = baker.make(BookGenre, slug="genre1")
     genre2 = baker.make(BookGenre, slug="genre2")
-    book1 = baker.make(Book, title="Book 1", type=type1, genre=genre1, status="reading")
-    book2 = baker.make(Book, title="Book 2", type=type2, genre=genre2, status="reading")
-    return type1, type2, genre1, genre2, book1, book2
+    genre3 = baker.make(BookGenre, slug="genre3", parent=genre1)
+    location1 = baker.make(BookLocation, slug="location1")
+    location2 = baker.make(BookLocation, slug="location2")
+    book1 = baker.make(
+        Book,
+        title="Book 1",
+        type=type1,
+        genre=genre1,
+        status="reading",
+    )
+    book1.location.add(location1)
+    book2 = baker.make(
+        Book,
+        title="Book 2",
+        type=type2,
+        genre=genre2,
+        status="reading",
+    )
+    book2.location.add(location2)
+    book3 = baker.make(
+        Book,
+        title="Book 3",
+        type=type2,
+        genre=genre3,
+        status="reading",
+    )
+    return (
+        type1,
+        type2,
+        genre1,
+        genre2,
+        genre3,
+        location1,
+        location2,
+        book1,
+        book2,
+        book3,
+    )
 
 
 @pytest.mark.django_db
-def test_book_filtering_by_type_and_genre(
+def test_book_filtering_by_type_genre(
     client_logged_in, book_data, setup_staticfiles_storage
 ):
-    type1, type2, genre1, genre2, book1, book2 = book_data
+    (
+        type1,
+        type2,
+        genre1,
+        genre2,
+        genre3,
+        location1,
+        location2,
+        book1,
+        book2,
+        book3,
+    ) = book_data
     client, user = client_logged_in
     response = client.get(
-        reverse("status", args=("reading",)), {"type": "type1", "genre": "genre1"}
+        reverse("status", args=("reading",)),
+        {"type": type1.slug, "genre": genre1.slug},
     )
     assert response.status_code == 200
     assert book1 in [book for book, form in response.context["forms"]]
     assert book2 not in [book for book, form in response.context["forms"]]
+
+
+@pytest.mark.django_db
+def test_book_filtering_by_location(
+    client_logged_in, book_data, setup_staticfiles_storage
+):
+    (
+        type1,
+        type2,
+        genre1,
+        genre2,
+        genre3,
+        location1,
+        location2,
+        book1,
+        book2,
+        book3,
+    ) = book_data
+    client, user = client_logged_in
+    response = client.get(
+        reverse("status", args=("reading",)),
+        {"location": location1.slug},
+    )
+    assert response.status_code == 200
+    assert book1 in [book for book, form in response.context["forms"]]
+    assert book2 not in [book for book, form in response.context["forms"]]
+
+
+@pytest.mark.django_db
+def test_filtering_by_subgenre_and_subtype(
+    client_logged_in, book_data, setup_staticfiles_storage
+):
+    (
+        type1,
+        type2,
+        genre1,
+        genre2,
+        genre3,
+        location1,
+        location2,
+        book1,
+        book2,
+        book3,
+    ) = book_data
+    client, user = client_logged_in
+    response = client.get(
+        reverse("status", args=("reading",)),
+        {"type": type2.slug, "genre": genre3.slug},
+    )
+    assert response.status_code == 200
+    assert book1 not in [book for book, form in response.context["forms"]]
+    assert book3 in [book for book, form in response.context["forms"]]
