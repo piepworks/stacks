f03589dcd73078a6126c8ccffed538f8ea84dc95
Trey Piepmeier <trey@treypiepmeier.com>
2024-04-23 18:35:13 -0400

    Show "Today" if days ago is zero

--- core/templatetags/days_ago.py
+++ core/templatetags/days_ago.py
@@ -5,11 +5,13 @@ register = template.Library()
 
 
 @register.filter
-def days_since(value):
+def days_ago(value):
     now = datetime.now().date()  # Get the current date
     diff = now - value
     days = diff.days
     if days == 1:
         return "1 day ago"
+    elif days == 0:
+        return "Today"
     else:
         return f"{days} days ago"
