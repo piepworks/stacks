ab337f09a6dacfa301a805f40d965269c74c1585
Trey Piepmeier <trey@treypiepmeier.com>
2024-04-28 09:49:49 -0400

    Remove slug fields altogether
    Everyone: "REMOVE SLUG FIELDS!"

--- core/models.py
+++ core/models.py
@@ -68,13 +68,12 @@ class User(AbstractUser):
 class Author(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="authors")
-    slug = models.SlugField()
     bio = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        unique_together = (("user", "slug"),)
+        unique_together = (("user", "name"),)
 
     def __str__(self):
         return self.name
@@ -135,7 +134,6 @@ class BookLocation(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="books")
-    slug = models.SlugField()
     author = models.ManyToManyField(Author)
     published_year = models.IntegerField(blank=True, null=True)
     status = models.CharField(
@@ -180,7 +178,7 @@ class Book(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        unique_together = (("user", "slug"),)
+        unique_together = (("user", "title"),)
 
     def __str__(self):
         return self.title
