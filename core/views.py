13ff3a6714101c078e35553cfe2c80c9f3595551
Trey Piepmeier <trey@treypiepmeier.com>
2024-05-10 19:59:28 -0500

    Redirect to note section when you add a note
    This was a stopgap since I removed Alpine AJAX, but I think it's better.

--- core/views.py
+++ core/views.py
@@ -643,7 +643,7 @@ def note_new(request, pk):
             note.book = book
             note.save()
             messages.success(request, "Note added")
-            return redirect(book.get_absolute_url())
+            return redirect(book.get_absolute_url() + "#heading-notes")
 
     else:
         form = BookNoteForm()
