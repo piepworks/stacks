3e4cef60dc3406b9aa51064d63c31b2b8f7ba234
Trey Piepmeier <trey@treypiepmeier.com>
2024-05-17 07:46:34 -0400

    Get a handle on default Tailwind layout utilities
    https://tailwindcss.com/docs/container
    
    I had incorrectly assumed that daisyUI would have stuff, but they
    clearly say that they lean on the default Tailwind stuff.
    
    > Layout, sizing, grids, spacing, etc. all will be handled by Tailwind
    > CSS's utility classes.
    
    https://daisyui.com/docs/layout-and-typography/

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,14 @@ module.exports = {
     '**/templates/**/*.{html,js,svg}',
     '**/core/templates/**/*.{html,js,svg}',
   ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+      },
+    },
+  },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
