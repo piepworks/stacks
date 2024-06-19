8a6cfe79b0dd0ab7236e948683640adeeff3a7ff
Trey Piepmeier <trey@treypiepmeier.com>
2024-06-18 21:38:21 -0400

    Have Celery use its own database

--- config/settings.py
+++ config/settings.py
@@ -225,4 +225,4 @@ HONEYPOT_FIELD_NAME = "phonenumber"
 # django-registration
 ACCOUNT_ACTIVATION_DAYS = 2
 
-CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="sqla+sqlite:///db.sqlite3")
+CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="sqla+sqlite:///db-celery.sqlite3")
