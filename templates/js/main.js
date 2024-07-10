537e555f54c0bac68ec06a4518744aab3ae88c63
Trey Piepmeier <trey@treypiepmeier.com>
2024-07-09 20:00:03 -0400

    Ignore "/" key shortcut when entering text

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,6 +1,11 @@
 document.body.removeEventListener('keyup', window.bsKeyupSlash);
 window.bsKeyupSlash = (e) => {
-  if (e.code === 'Slash') {
+  if (
+    e.code === 'Slash' &&
+    document.activeElement.tagName !== 'INPUT' &&
+    document.activeElement.tagName !== 'SELECT' &&
+    document.activeElement.tagName !== 'TEXTAREA'
+  ) {
     const searchForm = document.querySelector('.search-form form input');
     searchForm.focus();
   }
