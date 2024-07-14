310aafa66def45c05bb24e2e226f5d81eb2c3760
Trey Piepmeier <trey@treypiepmeier.com>
2024-07-14 19:20:46 -0400

    Un-break non-genre filter counts

--- core/filter_helpers.py
+++ core/filter_helpers.py
@@ -1,13 +1,17 @@
-def calculate_unique_child_counts(parent, items, queryset):
-    # `set()`s in Python only store unique elements
-    unique_books = set()
+def calculate_unique_child_counts(parent, items, queryset, field_name, child_counts):
+    if field_name == "genre":
+        unique_books = set()
 
-    for child in items:
-        if child.parent == parent:
-            related_books = queryset.filter(genre__slug=child.slug)
-            unique_books.update({book.id for book in related_books})
+        for child in items:
+            if child.parent == parent:
+                related_books = queryset.filter(genre__slug=child.slug)
+                unique_books.update({book.id for book in related_books})
 
-    return len(unique_books)
+        return len(unique_books)
+    else:
+        return sum(
+            child_counts[child.slug] for child in items if child.parent == parent
+        )
 
 
 def get_filter_counts(queryset, items, field_name):
@@ -24,7 +28,9 @@ def get_filter_counts(queryset, items, field_name):
         parent_counts = {
             parent.slug: {
                 "count": queryset.filter(**{f"{field_name}__slug": parent.slug}).count()
-                + calculate_unique_child_counts(parent, items, queryset),
+                + calculate_unique_child_counts(
+                    parent, items, queryset, field_name, child_counts
+                ),
                 "sub_items": {
                     child.slug: child_counts[child.slug]
                     for child in items
